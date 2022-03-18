6d379b86a013858c0cea6b966761cf86da311189
Aditya Kurhade <adikrhd@gmail.com>
2022-03-18 19:58:05 +0530

    infinite scroller added.

--- src/News/News.js
+++ src/News/News.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import NewsItem from './NewsItem'
 
 export class News extends Component {
@@ -9,49 +10,63 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            allArticles:[],
+            allArticles: [],
             loading: true,
             page: 1,
-            disableNext : false,
-            totalPages : 0
+            disableNext: false,
+            totalPages: 0
         }
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         let url = `https://saurav.tech/NewsAPI/top-headlines/category/${this.props.category}/${this.props.countryCode}.json`;
-        let data= await fetch(url);
+        let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({articles : parsedData.articles.slice(0,this.pageSize) ,
-             allArticles:parsedData.articles,
-             loading : false ,
-              totalPages : Math.ceil(parsedData.articles.length/this.pageSize),
-              page:1});
-              window.scrollTo(0, 0)      
-    }
-
-    handlePrev=()=>{
-        this.setState({loading : true});
+        this.setState({
+            articles: parsedData.articles.slice(0, this.pageSize),
+            allArticles: parsedData.articles,
+            loading: false,
+            totalPages: Math.ceil(parsedData.articles.length / this.pageSize),
+            page: 1
+        });
         window.scrollTo(0, 0)
-        setTimeout(()=> {
-            let newPage = this.state.page - 1;
-            this.setState({articles : this.state.allArticles.slice((newPage-1)*this.pageSize,newPage*this.pageSize) , loading : false , page : newPage,disableNext: false })
-          }, 1000);
     }
 
-    handleNext= ()=>{
+    handlePrev = () => {
+        this.setState({ loading: true });
+        window.scrollTo(0, 0)
+        setTimeout(() => {
+            let newPage = this.state.page - 1;
+            this.setState({ articles: this.state.allArticles.slice((newPage - 1) * this.pageSize, newPage * this.pageSize), loading: false, page: newPage, disableNext: false })
+        }, 1000);
+    }
 
-        if(this.state.page + 1 <= this.state.totalPages){
-            this.setState({loading : true});
+    handleNext = () => {
+
+        if (this.state.page + 1 <= this.state.totalPages) {
+            this.setState({ loading: true });
             window.scrollTo(0, 0)
-            setTimeout(()=> {
+            setTimeout(() => {
                 let newPage = this.state.page + 1;
                 let disableNext = newPage === this.state.totalPages ? true : false;
-                this.setState({articles : this.state.allArticles.slice((newPage-1)*this.pageSize,newPage*this.pageSize) , loading : false , page : newPage ,disableNext: disableNext })
-              }, 1000);
-            
+                this.setState({ articles: this.state.allArticles.slice((newPage - 1) * this.pageSize, newPage * this.pageSize), loading: false, page: newPage, disableNext: disableNext })
+            }, 1000);
+
         }
-        
+
     }
+
+    fetchMoreData = () => {
+        setTimeout(() => {
+            let newPage = this.state.page + 1;
+            this.setState({
+                page: newPage,
+                articles: this.state.articles.concat(this.state.allArticles.slice((newPage - 1) * this.pageSize, newPage * this.pageSize))
+            });
+
+
+        }, 1500);
+    };
 
     render() {
         return (
@@ -67,22 +82,36 @@ export class News extends Component {
                         <>
                             <div>
 
-                                <div className="container my-3">
-                                    <div className="row">
-                                        {this.state.articles.map((article) => {
 
-                                            return <div className="col-md-4" key={article.publishedAt}>
-                                                <NewsItem title={article.title} description={article.description}
-                                                    imageUrl={article.urlToImage} url={article.url} author={article.author} date={article.publishedAt} source={article.source.name} />
-                                            </div>
-                                        })}
 
+                                <InfiniteScroll
+                                    dataLength={this.state.articles.length}
+                                    next={this.fetchMoreData}
+                                    hasMore={this.state.articles.length !== this.state.allArticles.length}
+                                    loader={<div className="text-center">
+                                        <div className="spinner-border" role="status">
+                                            <span className="visually-hidden">Loading...</span>
+                                        </div>
+                                    </div>}
+                                >
+                                    <div className="container">
+                                        <div className="row">
+                                            {this.state.articles.map((article) => {
+
+                                                return <div className="col-md-4" key={article.publishedAt}>
+                                                    <NewsItem title={article.title} description={article.description}
+                                                        imageUrl={article.urlToImage} url={article.url} author={article.author} date={article.publishedAt} source={article.source.name} />
+                                                </div>
+                                            })}
+
+                                        </div>
                                     </div>
-                                    <div className="container d-flex justify-content-between">
+                                </InfiniteScroll>
+                                {/* <div className="container d-flex justify-content-between">
                                         <button type="button" disabled={this.state.page === 1} onClick={this.handlePrev} className="btn btn-primary">&larr; Prev</button>
                                         <button type="button" disabled={this.state.disableNext} className="btn btn-primary" onClick={this.handleNext}>Next &rarr;</button>
-                                    </div>
-                                </div>
+                                    </div> */}
+
                             </div>
 
                         </>
